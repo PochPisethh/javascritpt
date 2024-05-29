2d6bd5c4fdd14a83e00a7a0589893188841677ea
PochPisethh <pochpiseth971@gmail.com>
2024-05-29 12:00:32 +0700

    render product

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+import { CardComponent } from "../components/CardComponent.js";
+import { products } from "../data/products.js";
+let renderArea = document.querySelector("#renderArea");
+
+// RENDER PRODUCT
+products
+  .map((product) => (renderArea.innerHTML += CardComponent(product)));
